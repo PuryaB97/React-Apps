2cab967ec5e0cbe9c7a79dca488d30141c46bb51
Purya Behdarvandan <purya.B97@gmail.com>
2022-10-01 23:27:36 +0300

    Routes

--- movie-app/src/App.js
+++ movie-app/src/App.js
@@ -1,10 +1,27 @@
 import "./App.scss";
+import "swiper/swiper.min.css";
+import "./assets/boxicons-2.0.7/css/boxicons.min.css";
+
+import { BrowserRouter, Route } from "react-router-dom";
+
+import Header from "./components/header/Header";
+import Footer from "./components/footer/Footer";
+
+import Routes from "./config/Routes";
 
 function App() {
   return (
-    <div className="App">
-      <h1>Hello World!</h1>
-    </div>
+    <BrowserRouter>
+      <Route
+        render={(props) => (
+          <>
+            <Header {...props} />
+            <Routes />
+            <Footer />
+          </>
+        )}
+      />
+    </BrowserRouter>
   );
 }
 
