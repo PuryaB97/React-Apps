16a461cad4c51316a5257a1412e0baf8dd4a6505
Purya Behdarvandan <purya.B97@gmail.com>
2022-10-02 23:51:31 +0300

    Hero slide

--- movie-app/src/pages/Home.jsx
+++ movie-app/src/pages/Home.jsx
@@ -1,7 +1,13 @@
 import React from "react";
 
+import HeroSlide from "../components/hero-slide/HeroSlide";
+
 const Home = () => {
-  return <div>Home</div>;
+  return (
+    <div>
+      <HeroSlide />
+    </div>
+  );
 };
 
 export default Home;
