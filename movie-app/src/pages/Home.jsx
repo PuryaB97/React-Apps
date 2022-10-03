ca2e17fd73d7f61cbc35c260eb9b7b386b066b08
Purya Behdarvandan <purya.B97@gmail.com>
2022-10-03 23:50:18 +0300

    Top rated

--- movie-app/src/pages/Home.jsx
+++ movie-app/src/pages/Home.jsx
@@ -21,6 +21,36 @@ const Home = () => {
           </div>
           <MovieList category={category.movie} type={movieType.popular} />
         </div>
+
+        <div className="section mb-3">
+          <div className="section__header mb-2">
+            <h2>Top Rated Movies</h2>
+            <Link>
+              <OutlineButton className="small">View more</OutlineButton>
+            </Link>
+          </div>
+          <MovieList category={category.movie} type={movieType.top_rated} />
+        </div>
+
+        <div className="section mb-3">
+          <div className="section__header mb-2">
+            <h2>Trending TV</h2>
+            <Link>
+              <OutlineButton className="small">View more</OutlineButton>
+            </Link>
+          </div>
+          <MovieList category={category.tv} type={tvType.popular} />
+        </div>
+
+        <div className="section mb-3">
+          <div className="section__header mb-2">
+            <h2>Top Rated TV</h2>
+            <Link>
+              <OutlineButton className="small">View more</OutlineButton>
+            </Link>
+          </div>
+          <MovieList category={category.tv} type={tvType.top_rated} />
+        </div>
       </div>
     </div>
   );
