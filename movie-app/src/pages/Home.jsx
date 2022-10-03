d13ac9b9a88cfe2a2e11cfa07c6e4a73bb7ff2fd
Purya Behdarvandan <purya.B97@gmail.com>
2022-10-03 23:29:21 +0300

    Making Home page

--- movie-app/src/pages/Home.jsx
+++ movie-app/src/pages/Home.jsx
@@ -1,4 +1,6 @@
 import React from "react";
+import { Link } from "react-router-dom";
+import { OutlineButton } from "../components/button/Button";
 
 import HeroSlide from "../components/hero-slide/HeroSlide";
 
@@ -6,6 +8,16 @@ const Home = () => {
   return (
     <div>
       <HeroSlide />
+      <div className="container">
+        <div className="section mb-3">
+          <div className="section__header mb-2">
+            <h2>Trending Movies</h2>
+            <Link>
+              <OutlineButton className="small">View more</OutlineButton>
+            </Link>
+          </div>
+        </div>
+      </div>
     </div>
   );
 };
