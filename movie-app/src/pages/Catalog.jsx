a7a12c7d91b35ad81a9a1bac7dc038a0c47f00b4
Purya Behdarvandan <purya.B97@gmail.com>
2022-10-04 23:35:39 +0300

    Catalog page

--- movie-app/src/pages/Catalog.jsx
+++ movie-app/src/pages/Catalog.jsx
@@ -1,7 +1,19 @@
 import React from "react";
 
+import { useParams } from "react-router-dom";
+
+import PageHeader from "../components/page-header/PageHeader";
+
+import { category as cate } from "../api/tmdbApi";
+
 const Catalog = () => {
-  return <div>Catalog</div>;
+  const { category } = useParams();
+
+  return (
+    <>
+      <PageHeader>{category === cate.movie ? "Movie" : "TV Series"}</PageHeader>
+    </>
+  );
 };
 
 export default Catalog;
