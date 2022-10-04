b6dced2de0aab67d97227a51d671b63ef1fb9794
Purya Behdarvandan <purya.B97@gmail.com>
2022-10-04 23:56:54 +0300

    Movie grid

---
+++ movie-app/src/components/movie-grid/MovieGrid.jsx
@@ -0,0 +1,88 @@
+import React, { useState, useEffect } from "react";
+
+import "./movie-grid.scss";
+
+import MovieCard from "../movie-card/MovieCard";
+import { useParams } from "react-router-dom";
+import tmdbApi, { category, movieType, tvType } from "../../api/tmdbApi";
+import { OutlineButton } from "../button/Button";
+
+const MovieGrid = (props) => {
+  const [items, setItems] = useState([]);
+
+  const [page, setPage] = useState(1);
+  const [totalPage, setTotalPage] = useState(0);
+
+  const { keyword } = useParams();
+
+  useEffect(() => {
+    const getList = async () => {
+      let response = null;
+      if (keyword === undefined) {
+        const params = {};
+        switch (props.category) {
+          case category.movie:
+            response = await tmdbApi.getMoviesList(movieType.upcoming, {
+              params,
+            });
+            break;
+          default:
+            response = await tmdbApi.getTvList(tvType.popular, { params });
+        }
+      } else {
+        const params = {
+          query: keyword,
+        };
+        response = await tmdbApi.search(props.category, { params });
+      }
+      setItems(response.results);
+      setTotalPage(response.total_pages);
+    };
+    getList();
+  }, [props.category, keyword]);
+
+  const loadMore = async () => {
+    let response = null;
+    if (keyword === undefined) {
+      const params = {
+        page: page + 1,
+      };
+      switch (props.category) {
+        case category.movie:
+          response = await tmdbApi.getMoviesList(movieType.upcoming, {
+            params,
+          });
+          break;
+        default:
+          response = await tmdbApi.getTvList(tvType.popular, { params });
+      }
+    } else {
+      const params = {
+        page: page + 1,
+        query: keyword,
+      };
+      response = await tmdbApi.search(props.category, { params });
+    }
+    setItems([...items, ...response.results]);
+    setPage(page + 1);
+  };
+
+  return (
+    <>
+      <div className="movie-grid">
+        {items.map((item, i) => (
+          <MovieCard category={props.category} item={item} key={i} />
+        ))}
+      </div>
+      {page < totalPage ? (
+        <div className="movie-grid__loadmore">
+          <OutlineButton className="small" onClick={loadMore}>
+            Load more
+          </OutlineButton>
+        </div>
+      ) : null}
+    </>
+  );
+};
+
+export default MovieGrid;
--- movie-app/src/pages/Catalog.jsx
+++ movie-app/src/pages/Catalog.jsx
@@ -3,6 +3,7 @@ import React from "react";
 import { useParams } from "react-router-dom";
 
 import PageHeader from "../components/page-header/PageHeader";
+import MovieGrid from "../components/movie-grid/MovieGrid";
 
 import { category as cate } from "../api/tmdbApi";
 
@@ -12,6 +13,11 @@ const Catalog = () => {
   return (
     <>
       <PageHeader>{category === cate.movie ? "Movie" : "TV Series"}</PageHeader>
+      <div className="container">
+        <div className="section mb-3">
+          <MovieGrid category={category} />
+        </div>
+      </div>
     </>
   );
 };
