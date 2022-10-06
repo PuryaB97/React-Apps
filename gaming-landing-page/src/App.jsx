7b08685db9059096ed09792cf6bae75c0f2860ce
Purya Behdarvandan <purya.B97@gmail.com>
2022-10-06 23:56:34 +0300

    Header

--- gaming-landing-page/src/App.jsx
+++ gaming-landing-page/src/App.jsx
@@ -1,8 +1,14 @@
 import { useState } from "react";
 import "./App.scss";
 
+import Header from "./components/header/Header";
+
 function App() {
-  return <div className="App">App</div>;
+  return (
+    <div className="App">
+      <Header />
+    </div>
+  );
 }
 
 export default App;
