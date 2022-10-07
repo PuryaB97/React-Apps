7ff3bc52ed93a6fb75004b5be92918a72fa74174
Purya Behdarvandan <purya.B97@gmail.com>
2022-10-08 00:36:53 +0300

    Auto image slide

--- gaming-landing-page/src/components/home-section/welcome/Welcome.jsx
+++ gaming-landing-page/src/components/home-section/welcome/Welcome.jsx
@@ -1,7 +1,10 @@
+import { useEffect } from "react";
 import "./welcome.scss";
 
 import HomeSection from "../HomeSection";
 import Button from "../../button/Button";
+
+import hoverEffect from "hover-effect";
 
 import {
   bg1,
@@ -14,6 +17,46 @@ import {
 const champImgs = [champAshe, champAhri, champGaren];
 
 const Welcome = (props) => {
+  useEffect(() => {
+    const welcomeImgs = document.querySelectorAll("#welcome__img__slide > img");
+    let animates = [];
+    welcomeImgs.forEach((item, index) => {
+      let nextImg =
+        welcomeImgs[
+          index === welcomeImgs.length - 1 ? 0 : index + 1
+        ].getAttribute("src");
+      let animation = new hoverEffect({
+        parent: document.querySelector("#welcome__img__slide"),
+        intensity: 0.5,
+        image1: item.getAttribute("src"),
+        image2: nextImg,
+        displacementImage: distortion,
+        hover: false,
+      });
+      animates.push(animation);
+    });
+    welcomeImgs.forEach((e) => e.remove());
+
+    let currItem = 0;
+
+    const autoImageSlide = () => {
+      let prevItem = currItem;
+      currItem = (currItem + 1) % animates.length;
+
+      if (!document.hidden) {
+        animates[prevItem].next();
+      }
+
+      setTimeout(() => {
+        let canvas = document.querySelectorAll("#welcome__img__slide > canvas");
+        document.querySelector("#welcome__img__slide").appendChild(canvas[0]);
+        animates[prevItem].previous();
+      }, 3000);
+    };
+
+    setInterval(autoImageSlide, 3000);
+  }, []);
+
   return (
     <HomeSection
       className={`welcome ${props.isActive ? "active" : ""}`}
@@ -40,7 +83,13 @@ const Welcome = (props) => {
           </div>
         </div>
       </div>
-      <div className="welcome__img relative"></div>
+      <div className="welcome__img relative">
+        <div className="welcome__img__slide" id="welcome__img__slide">
+          {champImgs.map((item, index) => (
+            <img src={item} key={index} />
+          ))}
+        </div>
+      </div>
     </HomeSection>
   );
 };
