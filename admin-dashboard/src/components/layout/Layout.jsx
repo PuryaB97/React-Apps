414a3bd717a7a34c8b59b53ccdab1b5be9a5e743
Purya Behdarvandan <purya.B97@gmail.com>
2022-10-12 23:44:11 +0300

    Downgrade the dependencies

--- admin-dashboard/src/components/layout/Layout.jsx
+++ admin-dashboard/src/components/layout/Layout.jsx
@@ -1,7 +1,27 @@
 import React from "react";
 
+import Sidebar from "../sidebar/Sidebar";
+import Routes from "../Routes";
+
+import { BrowserRouter, Route } from "react-router-dom";
+
 const Layout = () => {
-  return <div>Layout</div>;
+  return (
+    <BrowserRouter>
+      <Route
+        render={(props) => (
+          <div className="layout">
+            <Sidebar {...props} />
+            <div className="layout__content">
+              <div className="layout__content-main">
+                <Routes />
+              </div>
+            </div>
+          </div>
+        )}
+      />
+    </BrowserRouter>
+  );
 };
 
 export default Layout;
