68ef79a6c1859b7033742ca40b38c452923d108b
Purya Behdarvandan <purya.B97@gmail.com>
2022-10-12 23:30:41 +0300

    Layout

--- admin-dashboard/src/index.js
+++ admin-dashboard/src/index.js
@@ -1,13 +1,19 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import React from "react";
+import ReactDOM from "react-dom/client";
+import reportWebVitals from "./reportWebVitals";
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
+import "./assets/boxicons-2.0.7/css/boxicons.min.css";
+import "./assets/css/grid.css";
+import "./assets/css/index.css";
+
+import Layout from "./components/layout/Layout";
+
+document.title = "Pouriya";
+
+const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <React.StrictMode>
-    <App />
+    <Layout />
   </React.StrictMode>
 );
 
