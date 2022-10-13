9fe5d52fc606225980912b9d73c5b1a5dc183571
Purya Behdarvandan <purya.B97@gmail.com>
2022-10-13 23:56:24 +0300

    Top nav

--- admin-dashboard/src/components/layout/Layout.jsx
+++ admin-dashboard/src/components/layout/Layout.jsx
@@ -3,6 +3,7 @@ import React from "react";
 import "./layout.css";
 
 import Sidebar from "../sidebar/Sidebar";
+import TopNav from "../topnav/TopNav";
 import Routes from "../Routes";
 
 import { BrowserRouter, Route } from "react-router-dom";
@@ -15,6 +16,7 @@ const Layout = () => {
           <div className="layout">
             <Sidebar {...props} />
             <div className="layout__content">
+              <TopNav />
               <div className="layout__content-main">
                 <Routes />
               </div>
---
+++ admin-dashboard/src/components/topnav/TopNav.jsx
@@ -0,0 +1,22 @@
+import React from "react";
+
+import "./topnav.css";
+
+const TopNav = () => {
+  return (
+    <div className="topnav">
+      <div className="topnav__search">
+        <input type="text" placeholder="Search here..." />
+        <i class="bx bxs-virus-block"></i>
+      </div>
+      <div className="topnav__right">
+        <div className="topnav__right-item">{/* {dropsdown here} */}</div>
+        <div className="topnav__right-item">{/* {dropsdown here} */}</div>
+        <div className="topnav__right-item">{/* {dropsdown here} */}</div>
+        <div className="topnav__right-item">{/* {dropsdown here} */}</div>
+      </div>
+    </div>
+  );
+};
+
+export default TopNav;
