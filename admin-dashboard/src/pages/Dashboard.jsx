715f66397240f54e811338bc988225d684cfeb54
Purya Behdarvandan <purya.B97@gmail.com>
2022-10-15 23:28:48 +0300

    Dashboard page

--- admin-dashboard/src/pages/Dashboard.jsx
+++ admin-dashboard/src/pages/Dashboard.jsx
@@ -1,7 +1,22 @@
 import React from "react";
 
+import statusCards from "../assets/JsonData/status-card-data.json";
+
 const Dashboard = () => {
-  return <div>Dashboard</div>;
+  return (
+    <div>
+      <h2 className="page-header">Dashboard</h2>
+      <div className="row">
+        <div className="col-6">
+          <div className="row">
+            {statusCards.map((item, index) => (
+              <div className="col-6">{item.title}</div>
+            ))}
+          </div>
+        </div>
+      </div>
+    </div>
+  );
 };
 
 export default Dashboard;
