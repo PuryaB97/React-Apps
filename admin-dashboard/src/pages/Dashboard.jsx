b09f8afe68b2588c5cc14235c01decf1aac7170b
Purya Behdarvandan <purya.B97@gmail.com>
2022-10-15 23:50:39 +0300

    Add apexcharts

--- admin-dashboard/src/pages/Dashboard.jsx
+++ admin-dashboard/src/pages/Dashboard.jsx
@@ -1,8 +1,55 @@
 import React from "react";
 
+import Chart from "react-apexcharts";
+
 import StatusCard from "../components/status-card/StatusCard";
 
 import statusCards from "../assets/JsonData/status-card-data.json";
+import { Link } from "react-router-dom";
+
+const chartOptions = {
+  series: [
+    {
+      name: "Online Customers",
+      data: [40, 70, 20, 90, 36, 80, 30, 91, 60],
+    },
+    {
+      name: "Store Customers",
+      data: [40, 30, 70, 80, 40, 16, 40, 20, 51, 10],
+    },
+  ],
+  options: {
+    color: ["#6ab04c", "#2980b9"],
+    chart: {
+      background: "transparent",
+    },
+    dataLabels: {
+      enabled: false,
+    },
+    stroke: {
+      curve: "smooth",
+    },
+    xaxis: {
+      categories: [
+        "Jan",
+        "Feb",
+        "Mar",
+        "Apr",
+        "May",
+        "Jun",
+        "Jul",
+        "Aug",
+        "Sep",
+      ],
+    },
+    legend: {
+      position: "top",
+    },
+    grid: {
+      show: false,
+    },
+  },
+};
 
 const Dashboard = () => {
   return (
@@ -23,7 +70,25 @@ const Dashboard = () => {
           </div>
         </div>
         <div className="col-6">
-          <div className="card full-height"></div>
+          <div className="card full-height">
+            <Chart
+              options={chartOptions.options}
+              series={chartOptions.series}
+              type="line"
+              height="100%"
+            />
+          </div>
+        </div>
+        <div className="col-4">
+          <div className="card">
+            <div className="card__header">
+              <h3>top customers</h3>
+            </div>
+            <div className="card__body"></div>
+            <div className="card__footer">
+              <Link TO="/">view all</Link>
+            </div>
+          </div>
         </div>
       </div>
     </div>
