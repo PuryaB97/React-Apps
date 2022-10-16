5ecba327c1acf0b27c3dd1bf8ae6d5391dcdfc6c
Purya Behdarvandan <purya.B97@gmail.com>
2022-10-16 23:27:27 +0300

    Latest orders

---
+++ admin-dashboard/src/components/badge/Badge.jsx
@@ -0,0 +1,9 @@
+import React from "react";
+
+import "./bade.css";
+
+const Badge = () => {
+  return <div>Badge</div>;
+};
+
+export default Badge;
--- admin-dashboard/src/pages/Dashboard.jsx
+++ admin-dashboard/src/pages/Dashboard.jsx
@@ -6,6 +6,8 @@ import StatusCard from "../components/status-card/StatusCard";
 
 import Table from "../components/table/Table";
 
+import Badge from "../components/badge/Badge";
+
 import statusCards from "../assets/JsonData/status-card-data.json";
 
 import { Link } from "react-router-dom";
@@ -95,6 +97,68 @@ const renderCusomerBody = (item, index) => (
   </tr>
 );
 
+const latestOrders = {
+  header: ["order id", "user", "total price", "date", "status"],
+  body: [
+    {
+      id: "#OD1711",
+      user: "john doe",
+      date: "17 Jun 2021",
+      price: "$900",
+      status: "shipping",
+    },
+    {
+      id: "#OD1712",
+      user: "frank iva",
+      date: "1 Jun 2021",
+      price: "$400",
+      status: "paid",
+    },
+    {
+      id: "#OD1713",
+      user: "anthony baker",
+      date: "27 Jun 2021",
+      price: "$200",
+      status: "pending",
+    },
+    {
+      id: "#OD1712",
+      user: "frank iva",
+      date: "1 Jun 2021",
+      price: "$400",
+      status: "paid",
+    },
+    {
+      id: "#OD1713",
+      user: "anthony baker",
+      date: "27 Jun 2021",
+      price: "$200",
+      status: "refund",
+    },
+  ],
+};
+
+const orderStatus = {
+  shipping: "primary",
+  pending: "warning",
+  paid: "success",
+  refund: "danger",
+};
+
+const renderOrderHead = (item, index) => <th key={index}>{item}</th>;
+
+const renderOrderBody = (item, index) => (
+  <tr key={index}>
+    <td>{item.id}</td>
+    <td>{item.user}</td>
+    <td>{item.price}</td>
+    <td>{item.date}</td>
+    <td>
+      <Badge type={orderStatus[item.status]} content={item.status} />
+    </td>
+  </tr>
+);
+
 const Dashboard = () => {
   return (
     <div>
@@ -137,7 +201,25 @@ const Dashboard = () => {
               />
             </div>
             <div className="card__footer">
-              <Link TO="/">view all</Link>
+              <Link to="/">view all</Link>
+            </div>
+          </div>
+        </div>
+        <div className="col-8">
+          <div className="card">
+            <div className="card__header">
+              <h3>latest orders</h3>
+            </div>
+            <div className="card__body">
+              <Table
+                headData={latestOrders.header}
+                renderHead={(item, index) => renderOrderHead(item, index)}
+                bodyData={latestOrders.body}
+                renderBody={(item, index) => renderOrderBody(item, index)}
+              />
+            </div>
+            <div className="card__footer">
+              <Link to="/">view all</Link>
             </div>
           </div>
         </div>
