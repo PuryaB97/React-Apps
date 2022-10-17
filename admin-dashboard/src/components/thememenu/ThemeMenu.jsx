9264e56e31d9fc723e4ed0e71134fbdfe1e5617c
Purya Behdarvandan <purya.B97@gmail.com>
2022-10-17 23:56:48 +0300

    Theme select

--- admin-dashboard/src/components/thememenu/ThemeMenu.jsx
+++ admin-dashboard/src/components/thememenu/ThemeMenu.jsx
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useEffect, useRef, useState } from "react";
 
 import "./thememenu.css";
 
@@ -50,23 +50,79 @@ const color_settings = [
   },
 ];
 
+const clickOutsideRef = (content_ref, toggle_ref) => {
+  document.addEventListener("mousedown", (e) => {
+    if (toggle_ref.current && toggle_ref.current.contains(e.target)) {
+      content_ref.current.classList.toggle("active");
+    } else {
+      if (content_ref.current && !content_ref.current.contains(e.target)) {
+        content_ref.current.classList.remove("active");
+      }
+    }
+  });
+};
+
 const ThemeMenu = () => {
+  const menu_ref = useRef(null);
+  const menu_toggle_ref = useRef(null);
+
+  clickOutsideRef(menu_ref, menu_toggle_ref);
+
+  const setActiveMenu = () => menu_ref.current.classList.add("active");
+  const closeMenu = () => menu_ref.current.classList.remove("active");
+
+  const [currMode, setCurrMode] = useState("light");
+
+  const [currColor, setCurrColor] = useState("blue");
+
+  const setMode = (mode) => {
+    setCurrMode(mode.id);
+    localStorage.setItem("themeMode", mode.class);
+  };
+
+  const setColor = (color) => {
+    setCurrColor(color.id);
+    localStorage.setItem("colorMode", color.class);
+  };
+
+  useEffect(() => {
+    const themeClass = mode_settings.find(
+      (e) => e.class === localStorage.getItem("themeMode", "theme-mode-light")
+    );
+
+    const colorClass = color_settings.find(
+      (e) => e.class === localStorage.getItem("colorMode", "theme-mode-light")
+    );
+
+    if (themeClass !== undefined) setCurrMode(themeClass.id);
+
+    if (colorClass !== undefined) setCurrColor(colorClass.id);
+  }, []);
+
   return (
     <div>
-      <button className="dropdown__toggle">
+      <button
+        ref={menu_toggle_ref}
+        className="dropdown__toggle"
+        onClick={() => setActiveMenu()}
+      >
         <i className="bx bx-palette"></i>
       </button>
-      <div className="theme-menu">
+      <div ref={menu_ref} className="theme-menu">
         <h4>Theme settings</h4>
-        <button className="theme-menu__close">
+        <button className="theme-menu__close" onClick={() => closeMenu()}>
           <i className="bx bx-x"></i>
         </button>
         <div className="theme-menu__select">
           <span>Choose mode</span>
           <ul className="mode-list">
             {mode_settings.map((item, index) => (
-              <li key={index}>
-                <div className={`mode-list__color ${item.background}`}>
+              <li key={index} onClick={() => setMode(item)}>
+                <div
+                  className={`mode-list__color ${item.background} ${
+                    item.id === currMode ? "active" : ""
+                  }`}
+                >
                   <i className="bx bx-check"></i>
                 </div>
                 <span>{item.name}</span>
@@ -78,8 +134,12 @@ const ThemeMenu = () => {
           <span>Choose color</span>
           <ul className="mode-list">
             {color_settings.map((item, index) => (
-              <li key={index}>
-                <div className={`mode-list__color ${item.background}`}>
+              <li key={index} onClick={() => setColor(item)}>
+                <div
+                  className={`mode-list__color ${item.background} ${
+                    item.id === currColor ? "active" : ""
+                  }`}
+                >
                   <i className="bx bx-check"></i>
                 </div>
                 <span>{item.name}</span>
