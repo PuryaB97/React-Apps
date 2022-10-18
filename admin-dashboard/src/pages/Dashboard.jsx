ff853ec00f51d6510373f203b3e0cd65943d6c71
Purya Behdarvandan <purya.B97@gmail.com>
2022-10-18 23:56:49 +0300

    Update Darkmode to Apexcharts

--- admin-dashboard/src/pages/Dashboard.jsx
+++ admin-dashboard/src/pages/Dashboard.jsx
@@ -1,8 +1,10 @@
-import React from "react";
+import React, { useEffect } from "react";
 
 import { Link } from "react-router-dom";
 
 import Chart from "react-apexcharts";
+
+import { useSelector, useDispatch } from "react-redux";
 
 import StatusCard from "../components/status-card/StatusCard";
 
@@ -11,6 +13,8 @@ import Table from "../components/table/Table";
 import Badge from "../components/badge/Badge";
 
 import statusCards from "../assets/JsonData/status-card-data.json";
+
+import ThemeAction from "../redux/actions/ThemeAction";
 
 const chartOptions = {
   series: [
@@ -160,6 +164,14 @@ const renderOrderBody = (item, index) => (
 );
 
 const Dashboard = () => {
+  const themeReducer = useSelector((state) => state.ThemeReducer.mode);
+
+  const dispatch = useDispatch();
+
+  useEffect(() => {
+    dispatch(ThemeAction.getTheme());
+  }, []);
+
   return (
     <div>
       <h2 className="page-header">Dashboard</h2>
@@ -180,7 +192,17 @@ const Dashboard = () => {
         <div className="col-6">
           <div className="card full-height">
             <Chart
-              options={chartOptions.options}
+              options={
+                themeReducer === "theme-mode-dark"
+                  ? {
+                      ...chartOptions.options,
+                      theme: { mode: "dark" },
+                    }
+                  : {
+                      ...chartOptions.options,
+                      theme: { mode: "light" },
+                    }
+              }
               series={chartOptions.series}
               type="line"
               height="100%"
