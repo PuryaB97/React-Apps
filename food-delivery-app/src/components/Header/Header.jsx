9a8ab6fefb5a25ece9ace00c0b4baa51f3a0a946
Purya Behdarvandan <purya.B97@gmail.com>
2022-10-20 23:11:52 +0300

    Right navs

--- food-delivery-app/src/components/Header/Header.jsx
+++ food-delivery-app/src/components/Header/Header.jsx
@@ -32,20 +32,38 @@ const Header = () => {
   return (
     <header className="header">
       <Container>
-        <div className="nav__wrapper">
+        <div className="nav__wrapper d-flex align-items-center justify-content-between">
           <div className="logo">
             <img src={logo} alt="logo" />
             <h5>Tasty Treat</h5>
           </div>
 
           <div className="navigation">
-            <div className="menu">
+            <div className="menu d-flex align-items-center gap-5">
               {nav__links.map((item, index) => (
                 <NavLink to={item.path} key={index}>
                   {item.display}
                 </NavLink>
               ))}
             </div>
+          </div>
+
+          {/* nav right icons */}
+          <div className="nav__right d-flex align-items-center gap-4">
+            <span className="cart__icon">
+              <i class="ri-shopping-basket-line"></i>
+              <span className="cart__badge">2</span>
+            </span>
+
+            <span className="user">
+              <Link to="/login">
+                <i class="ri-user-line"></i>
+              </Link>
+            </span>
+
+            <span className="mobile__menu">
+              <i class="ri-menu-line"></i>
+            </span>
           </div>
         </div>
       </Container>
