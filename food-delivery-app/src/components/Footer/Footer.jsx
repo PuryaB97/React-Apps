791f230b1aab9f74cda7ea961cafc625d6b08b8b
Purya Behdarvandan <purya.B97@gmail.com>
2022-10-20 23:55:54 +0300

    Creating footer

--- food-delivery-app/src/components/Footer/Footer.jsx
+++ food-delivery-app/src/components/Footer/Footer.jsx
@@ -1,7 +1,42 @@
 import React from "react";
+import { Container, Row, Col, ListGroup, ListGroupItem } from "reactstrap";
+import logo from "../../assets/images/res-logo.png";
+import "../../styles/footer.css";
 
 const Footer = () => {
-  return <div>Footer</div>;
+  return (
+    <footer className="footer">
+      <Container>
+        <Row>
+          <Col lg="3" md="4" sm="6">
+            <div className="logo footer__logo text-start">
+              <img src={logo} alt="logo" />
+              <h5>Tasty Treat</h5>
+              <p>
+                Lorem, ipsum dolor sit amet consectetur adipisicing elit. Iure
+                aliquid
+              </p>
+            </div>
+          </Col>
+          <Col lg="3" md="4" sm="6">
+            <h5>Delivery Time</h5>
+            <ListGroup>
+              <ListGroupItem className="delivery__time-item border-0 ps-0">
+                <span>Sunday - Thursday</span>
+                <p>10:00am - 11:00px</p>
+              </ListGroupItem>
+              <ListGroupItem className="delivery__time-item border-0 ps-0">
+                <span>Friday - Sturday</span>
+                <p>Off day</p>
+              </ListGroupItem>
+            </ListGroup>
+          </Col>
+          <Col lg="3" md="4" sm="6"></Col>
+          <Col lg="3" md="4" sm="6"></Col>
+        </Row>
+      </Container>
+    </footer>
+  );
 };
 
 export default Footer;
