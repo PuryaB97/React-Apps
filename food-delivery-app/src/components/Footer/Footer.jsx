6256ed27cdf506973df873e4da583fa572144950
Purya Behdarvandan <purya.B97@gmail.com>
2022-10-21 23:57:17 +0300

    Footer css

--- food-delivery-app/src/components/Footer/Footer.jsx
+++ food-delivery-app/src/components/Footer/Footer.jsx
@@ -3,6 +3,8 @@ import { Container, Row, Col, ListGroup, ListGroupItem } from "reactstrap";
 import logo from "../../assets/images/res-logo.png";
 import "../../styles/footer.css";
 
+import { Link } from "react-router-dom";
+
 const Footer = () => {
   return (
     <footer className="footer">
@@ -53,6 +55,30 @@ const Footer = () => {
               <input type="email" placeholder="Enter your email" />
               <span>
                 <i className="ri-send-plane-line"></i>
+              </span>
+            </div>
+          </Col>
+        </Row>
+
+        <Row className="mt-5">
+          <Col lg="6" md="6">
+            <p>
+              Copyright - 2022, website made by Pouriya Behdarvandan. All Rights
+              Reserved
+            </p>
+          </Col>
+          <Col lg="6" md="6">
+            <div className="social__links d-flex align-item-center gap-4 justify-content-end">
+              <p className="m-0">Follow: </p>
+              <span>
+                <Link to="https://twitter.com/Sophrosyne97">
+                  <i className="ri-twitter-fill"></i>
+                </Link>
+              </span>
+              <span>
+                <Link to="https://github.com/PuryaB97">
+                  <i className="ri-github-fill"></i>
+                </Link>
               </span>
             </div>
           </Col>
