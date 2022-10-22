1b9b734ad6f9d13a4d47a854de647377d23ed270
Purya Behdarvandan <purya.B97@gmail.com>
2022-10-22 23:46:02 +0300

    Home / hero section css

--- food-delivery-app/src/pages/Home.jsx
+++ food-delivery-app/src/pages/Home.jsx
@@ -6,6 +6,7 @@ import { Container, Row, Col } from "reactstrap";
 import heroImg from "../assets/images/hero.png";
 
 import "../styles/hero-section.css";
+import { Link } from "react-router-dom";
 
 const Home = () => {
   return (
@@ -16,14 +17,41 @@ const Home = () => {
             <Col lg="6" md="6">
               <div className="hero__content">
                 <h5 className="mb-3">Easy way to make an order</h5>
-                <h1 className="mb-4">
-                  <span>Hungry?</span> just wait food at<span> your door</span>
+                <h1 className="mb-4 hero__title">
+                  <span>Hungry?</span> just wait <br /> food at
+                  <span> your door</span>
                 </h1>
 
                 <p>
                   Lorem ipsum dolor sit, amet consectetur adipisicing elit.
                   Dolor repellendus ea quae, beatae debitis blanditiis?
                 </p>
+
+                <div className="hero__btns d-flex align-items-center gap-5 mt-4">
+                  <button className="order__btn d-flex align-items-center justify-content-between">
+                    Order now <i className="ri-arrow-right-s-line"></i>
+                  </button>
+
+                  <button className="all__foods-btn">
+                    <Link to="/foods">See all foods</Link>
+                  </button>
+                </div>
+
+                <div className="hero__service d-flex align-items-center gap-5 mt-5">
+                  <p className="d-flex align-items-center gap-2">
+                    <span className="shipping__icon">
+                      <i className="ri-car-line"></i>
+                    </span>{" "}
+                    No shipping charge
+                  </p>
+
+                  <p className="d-flex align-items-center gap-2">
+                    <span className="shipping__icon">
+                      <i className="ri-shield-check-line"></i>
+                    </span>{" "}
+                    100% secure checkout
+                  </p>
+                </div>
               </div>
             </Col>
 
