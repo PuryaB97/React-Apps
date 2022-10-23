cab08703d65bd55fcdab473bf93462d2fb0f8f4b
Purya Behdarvandan <purya.B97@gmail.com>
2022-10-23 23:26:52 +0300

    Feature section

--- food-delivery-app/src/pages/Home.jsx
+++ food-delivery-app/src/pages/Home.jsx
@@ -10,6 +10,30 @@ import { Link } from "react-router-dom";
 
 import Category from "../components/UI/category/Category";
 
+import "../styles/home.css";
+
+import featureImg01 from "../assets/images/service-01.png";
+import featureImg02 from "../assets/images/service-02.png";
+import featureImg03 from "../assets/images/service-03.png";
+
+const featureData = [
+  {
+    title: "Quick Delivery",
+    imgUrl: featureImg01,
+    desc: "Lorem, ipsum dolor sit amet consectetur adipisicing elit. Aspernatur, vero.",
+  },
+  {
+    title: "Super Dine In",
+    imgUrl: featureImg02,
+    desc: "Lorem, ipsum dolor sit amet consectetur adipisicing elit. Aspernatur, vero.",
+  },
+  {
+    title: "Easy Pick Up",
+    imgUrl: featureImg03,
+    desc: "Lorem, ipsum dolor sit amet consectetur adipisicing elit. Aspernatur, vero.",
+  },
+];
+
 const Home = () => {
   return (
     <Helmet title="Home">
@@ -68,6 +92,42 @@ const Home = () => {
 
       <section>
         <Category className="pt-0" />
+      </section>
+
+      <section>
+        <Container>
+          <Row>
+            <Col lg="12" className="text-center">
+              <h5 className="feature__subtitle mb-4">What we serve</h5>
+              <h2 className="feature__title">Just sit back at home</h2>
+              <h2 className="feature__title">
+                we will <span>take care</span>
+              </h2>
+              <p className="mb-1 mt-4 feature__text">
+                Lorem ipsum dolor sit amet consectetur, adipisicing elit. Animi,
+                error molestiae ex ea qui quis!
+              </p>
+              <p className="feature__text">
+                Lorem ipsum dolor sit amet consectetur adipisicing elit. Nihil
+                praesentium natus harum.
+              </p>
+            </Col>
+
+            {featureData.map((item, index) => (
+              <Col lg="4" md="4" key={index} className="mt-5">
+                <div className="feature__item text-center px-5 py-3">
+                  <img
+                    src={item.imgUrl}
+                    alt="feature-img"
+                    className="w-25 mb-3"
+                  />
+                  <h5 className="fw-bold mb-3">{item.title}</h5>
+                  <p>{item.desc}</p>
+                </div>
+              </Col>
+            ))}
+          </Row>
+        </Container>
       </section>
     </Helmet>
   );
