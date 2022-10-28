9f61072baa71481a4546765b487bf3176b9e6b42
Purya Behdarvandan <purya.B97@gmail.com>
2022-10-28 23:31:38 +0300

    cartSlice

---
+++ food-delivery-app/src/components/store/shopping-cart/cartSlice.js
@@ -0,0 +1,44 @@
+import { createSlice } from "@reduxjs/toolkit";
+
+const initialState = {
+  cartItems: [],
+  totalQuantity: 0,
+  totalAmount: 0,
+};
+
+const cartSlice = {
+  name: "cart",
+  initialState: initialState,
+
+  reducers: {
+    addItem(state, action) {
+      const newItem = action.payload;
+      const existingItem = state.cartItems.find(
+        (item) => item.id === newItem.id
+      );
+      state.totalAmount++;
+
+      if (!existingItem) {
+        state.cartItems.push({
+          id: newItem.id,
+          title: newItem.tite,
+          image01: newItem.image01,
+          price: newItem.price,
+          quantity: 1,
+          totalPrice: newItem.price,
+        });
+      } else {
+        existingItem.quantity++;
+        existingItem.totalPrice =
+          Number(existingItem.totalPrice) + Number(newItem.price);
+      }
+
+      state.totalAmount = state.cartItems.reduce(
+        (total, item) => total + Number(item.price) * Number(item.quantity)
+      );
+    },
+  },
+};
+
+export const cartActions = cartSlice.actions;
+export default cartSlice;
