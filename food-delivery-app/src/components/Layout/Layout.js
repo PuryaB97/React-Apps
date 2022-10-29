8498cc6a4cf7516bde71f0a438de0088a13779ab
Purya Behdarvandan <purya.B97@gmail.com>
2022-10-29 23:34:49 +0300

    Carts

--- food-delivery-app/src/components/Layout/Layout.js
+++ food-delivery-app/src/components/Layout/Layout.js
@@ -3,12 +3,15 @@ import React from "react";
 import Header from "../Header/Header";
 import Footer from "../Footer/Footer";
 import Routes from "../../routes/Routers";
-import { Route } from "react-router-dom";
+
+import Carts from "../UI/cart/Carts";
 
 const Layout = () => {
   return (
     <div>
       <Header />
+
+      <Carts />
       <div>
         <Routes />
       </div>
--- food-delivery-app/src/components/UI/cart/Carts.jsx
+++ food-delivery-app/src/components/UI/cart/Carts.jsx
@@ -1,9 +1,34 @@
 import React from "react";
 
 import { ListGroup } from "reactstrap";
+import { Link } from "react-router-dom";
+import CartItem from "./CartItem";
 
 const Carts = () => {
-  return <div>Carts</div>;
+  return (
+    <div className="cart__container">
+      <ListGroup className="cart">
+        <div className="cart__close">
+          <span>
+            <i className="ri-close-fill"></i>
+          </span>
+        </div>
+
+        <div className="cart__item-list">
+          <CartItem />
+        </div>
+
+        <div className="cart__bottom">
+          <h6>
+            Subtotal amount: <span>$123</span>
+          </h6>
+          <button>
+            <Link to="./checkout">Checkout</Link>
+          </button>
+        </div>
+      </ListGroup>
+    </div>
+  );
 };
 
 export default Carts;
