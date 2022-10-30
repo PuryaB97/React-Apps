4be502ec835c1e07c17d1f428d90782ec9b33f94
Purya Behdarvandan <purya.B97@gmail.com>
2022-10-31 00:18:12 +0300

    remove items reducer

--- food-delivery-app/src/components/UI/cart/CartItem.jsx
+++ food-delivery-app/src/components/UI/cart/CartItem.jsx
@@ -1,34 +1,59 @@
 import React from "react";
 import { ListGroupItem } from "reactstrap";
-import productImg from "../../../assets/images/product_01.1.jpg";
 
 import "../../../styles/cart-item.css";
 
-const CartItem = () => {
+import { useDispatch } from "react-redux";
+import { cartActions } from "../../store/shopping-cart/cartSlice";
+
+const CartItem = ({ item }) => {
+  const { id, title, price, image01, quantity, totalPrice } = item;
+
+  const dispatch = useDispatch();
+
+  const incrementItem = () => {
+    dispatch(
+      cartActions.addItem({
+        id,
+        title,
+        price,
+        image01,
+      })
+    );
+  };
+
+  const decreaseItem = () => {
+    dispatch(cartActions.removeItem(id));
+  };
+
+  const deleteItem = () => {
+    dispatch(cartActions.deleteItem(id));
+  };
+
   return (
     <ListGroupItem className="border-0 cart__item">
       <div className="cart__item-info d-flex gap-2">
-        <img src={productImg} alt="product-img" />
+        <img src={image01} alt="product-img" />
 
         <div className="cart__product-info w-100 d-flex align-items-center gap-4 justify-content-between">
           <div>
-            <h6 className="cart__product-title">Burger</h6>
+            <h6 className="cart__product-title">{title}</h6>
             <p className=" d-flex align-items-center gap-5 cart__product-price">
-              <span>$123</span>
+              {quantity}x <span>${totalPrice}</span>
             </p>
             <div className=" d-flex align-items-center justify-content-between increase__decrease-btn">
-              <span className="increase__btn">
-                <i className="ri-add-line"></i>
+              <span className="increase__btn" onClick={incrementItem}>
+                <i class="ri-add-line"></i>
               </span>
-              <span className="quantity">2</span>
-              <span className="decrease__btn">
-                <i className="ri-subtract-line"></i>
+              <span className="quantity">{quantity}</span>
+              <span className="decrease__btn" onClick={decreaseItem}>
+                <i class="ri-subtract-line"></i>
               </span>
             </div>
           </div>
 
-          <span className="delete__btn">
-            <i className="ri-close-line"></i>
+          <span className="delete__btn" onClick={deleteItem}>
+            <i class="ri-close-line"></i>
           </span>
         </div>
       </div>
--- food-delivery-app/src/components/UI/cart/Carts.jsx
+++ food-delivery-app/src/components/UI/cart/Carts.jsx
@@ -12,17 +12,17 @@ import "../../../styles/shopping-cart.css";
 const Carts = () => {
   const dispatch = useDispatch();
   const cartProducts = useSelector((state) => state.cart.cartItems);
+  const totalAmount = useSelector((state) => state.cart.totalAmount);
 
   const toggleCart = () => {
     dispatch(cartUiActions.toggle());
   };
-
   return (
     <div className="cart__container">
       <ListGroup className="cart">
         <div className="cart__close">
           <span onClick={toggleCart}>
-            <i className="ri-close-fill"></i>
+            <i class="ri-close-fill"></i>
           </span>
         </div>
 
@@ -30,18 +30,20 @@ const Carts = () => {
           {cartProducts.length === 0 ? (
             <h6 className="text-center mt-5">No item added to the cart</h6>
           ) : (
-            cartProducts.map((item, index) => {
-              <CartItem item={item} key={index} />;
-            })
+            cartProducts.map((item, index) => (
+              <CartItem item={item} key={index} />
+            ))
           )}
         </div>
 
-        <div className="cart__bottom">
+        <div className="cart__bottom d-flex align-items-center justify-content-between">
           <h6>
-            Subtotal amount: <span>$123</span>
+            Subtotal : <span>${totalAmount}</span>
           </h6>
           <button>
-            <Link to="./checkout">Checkout</Link>
+            <Link to="/checkout" onClick={toggleCart}>
+              Checkout
+            </Link>
           </button>
         </div>
       </ListGroup>
