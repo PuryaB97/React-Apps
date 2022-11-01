f5fe1ac36e4f94719a9db4487658511d16c81778
Purya Behdarvandan <purya.B97@gmail.com>
2022-11-01 23:40:44 +0300

    Sorting products func

--- food-delivery-app/src/pages/AllFoods.jsx
+++ food-delivery-app/src/pages/AllFoods.jsx
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 import Helmet from "../components/Helmet/Helmet";
 import CommonSection from "../components/UI/common-section/CommonSection";
 
@@ -10,6 +10,9 @@ import ProductCard from "../components/UI/product-card/ProductCart";
 import "../styles/all-foods.css";
 
 const AllFoods = () => {
+  const [searchTerm, setSearchTerm] = useState("");
+  const [productData, setProductData] = useState(products);
+
   return (
     <Helmet title="All Foods">
       <CommonSection title="All Foods" />
@@ -19,7 +22,12 @@ const AllFoods = () => {
           <Row>
             <Col lg="6" md="6" sm="6">
               <div className="search__widget d-flex align-items-center justify-content-between w-50">
-                <input type="text" placeholder="I'm looking for..." />
+                <input
+                  type="text"
+                  placeholder="I'm looking for..."
+                  value={searchTerm}
+                  onChange={(e) => setSearchTerm(e.target.value)}
+                />
                 <span>
                   <i className="ri-search-line"></i>
                 </span>
@@ -37,11 +45,17 @@ const AllFoods = () => {
               </div>
             </Col>
 
-            {products.map((item) => (
-              <Col lg="3" md="4" sm="6" xs="6">
-                <ProductCard item={item} key={item.id} className="mb-4" />
-              </Col>
-            ))}
+            {productData
+              .filter((item) => {
+                if (searchTerm.value === "") return item;
+                if (item.title.toLowerCase().includes(searchTerm.toLowerCase()))
+                  return item;
+              })
+              .map((item) => (
+                <Col lg="3" md="4" sm="6" xs="6">
+                  <ProductCard item={item} key={item.id} className="mb-4" />
+                </Col>
+              ))}
           </Row>
         </Container>
       </section>
