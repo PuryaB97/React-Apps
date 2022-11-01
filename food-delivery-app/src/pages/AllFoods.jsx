f0f4f8b5b66004cd8e4a8eb65d50372989b934ff
Purya Behdarvandan <purya.B97@gmail.com>
2022-11-01 23:25:23 +0300

    Foods page

--- food-delivery-app/src/pages/AllFoods.jsx
+++ food-delivery-app/src/pages/AllFoods.jsx
@@ -1,7 +1,50 @@
 import React from "react";
+import Helmet from "../components/Helmet/Helmet";
+import CommonSection from "../components/UI/common-section/CommonSection";
+
+import { Container, Row, Col } from "reactstrap";
+
+import products from "../assets/fake-data/products";
+import ProductCard from "../components/UI/product-card/ProductCart";
 
 const AllFoods = () => {
-  return <div>AllFoods</div>;
+  return (
+    <Helmet title="All Foods">
+      <CommonSection title="All Foods" />
+
+      <section>
+        <Container>
+          <Row>
+            <Col lg="6" md="6" sm="6">
+              <div className="search__widget">
+                <input type="text" placeholder="I'm looking for..." />
+                <span>
+                  <i className="ri-search-line"></i>
+                </span>
+              </div>
+            </Col>
+            <Col lg="6" md="6" sm="6" className="mb-5">
+              <div className="sorting__widget text-end">
+                <select>
+                  <option>Default</option>
+                  <option value="ascending">Alphabetically, A-Z</option>
+                  <option value="decending">Alphabetically, Z-A</option>
+                  <option value="high-price">High Price</option>
+                  <option value="low-price">Low Price</option>
+                </select>
+              </div>
+            </Col>
+
+            {products.map((item) => (
+              <Col lg="3" md="4" sm="6" xs="6">
+                <ProductCard item={item} key={item.id} className="mb-4" />
+              </Col>
+            ))}
+          </Row>
+        </Container>
+      </section>
+    </Helmet>
+  );
 };
 
 export default AllFoods;
